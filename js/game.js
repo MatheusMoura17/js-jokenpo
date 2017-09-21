81204e860e9f00ebb48e07211aa0b5ef7a85b72d
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-20 21:21:56 -0300

    feat: Game.js criado e lincado

---
+++ js/game.js
@@ -0,0 +1,13 @@
+var playerSelectedImage=document.GetElementById("player-selected-image");
+
+function PlayerHoverRock(){
+	playerSelectedImage.src="images/rock.png";
+}
+
+function PlayerHoverPaper(){
+
+}
+
+function PlayerHoverCutter(){
+
+}
