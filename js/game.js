0b5345cebd77dfbc28666f4d8c55890452bcdbd2
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-20 21:36:21 -0300

    feat: Seletor de maos implementado

--- js/game.js
+++ js/game.js
@@ -1,13 +1,53 @@
-var playerSelectedImage=document.GetElementById("player-selected-image");
+var selectedImage=false;
 
 function PlayerHoverRock(){
-	playerSelectedImage.src="images/rock.png";
+	if(!selectedImage){
+		var playerSelectedImage=document.getElementById("player-selected-image");
+		playerSelectedImage.src="images/rock-hover.png";
+	}
 }
 
 function PlayerHoverPaper(){
-
+	if(!selectedImage){
+		var playerSelectedImage=document.getElementById("player-selected-image");
+		playerSelectedImage.src="images/paper-hover.png";
+	}
 }
 
 function PlayerHoverCutter(){
+	if(!selectedImage){
+		var playerSelectedImage=document.getElementById("player-selected-image");
+		playerSelectedImage.src="images/cutter-hover.png";
+	}
+}
 
+function PlayerClickRock(){
+	if(!selectedImage){
+		selectedImage=true;
+		var playerSelectedImage=document.getElementById("player-selected-image");
+		playerSelectedImage.src="images/rock.png";
+	}
+}
+
+function PlayerClickPaper(){
+	if(!selectedImage){
+		selectedImage=true;
+		var playerSelectedImage=document.getElementById("player-selected-image");
+		playerSelectedImage.src="images/paper.png";
+	}
+}
+
+function PlayerClickCutter(){
+	if(!selectedImage){
+		selectedImage=true;
+		var playerSelectedImage=document.getElementById("player-selected-image");
+		playerSelectedImage.src="images/cutter.png";
+	}
+}
+
+function DefineInvisible(){
+	if(!selectedImage){
+		var playerSelectedImage=document.getElementById("player-selected-image");
+		playerSelectedImage.src="images/invisible.png";
+	}
 }
