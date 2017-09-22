ab738c429abf60a5c5d49f53c5421cd62bcb2827
Matheus Moura <matheus.moura@prodeaf.net>
2017-09-21 22:19:42 -0300

    feat: Animação de seleção de mao adicionada

--- js/game.js
+++ js/game.js
@@ -25,7 +25,9 @@ function PlayerClickRock(){
 	if(!selectedImage){
 		selectedImage=true;
 		var playerSelectedImage=document.getElementById("player-selected-image");
+		var view=document.getElementById("player-view");
 		playerSelectedImage.src="images/rock.png";
+		view.style="animation: select linear 0.1s";
 		RandomizeEnemy();
 	}
 }
@@ -57,20 +59,22 @@ function DefineInvisible(){
 
 function RandomizeEnemy(){
 	var enemySelectedImage=document.getElementById("enemy-selected-image");
+	var selected;
 	for(i=0;i<10;i++){
-		setTimeout(function(){
+		setTimeout(function(last){
 				var random=Math.floor((Math.random() * 3) + 1);
 				switch(random){
-					case 1:{
-						enemySelectedImage.src="images/rock-hover.png";
-					}break;
-					case 2:{
-						enemySelectedImage.src="images/paper-hover.png";
-					}break;
-					case 3:{
-						enemySelectedImage.src="images/cutter-hover.png";
-					}break;
+					case 1:	selected="rock";  break;
+					case 2: selected="paper"; break;
+					case 3: selected="cutter";break;
 				}
-		},i*250);
+				if(last){
+					enemySelectedImage.src="images/"+selected+".png";
+					var view=document.getElementById("enemy-view");
+					view.style="animation: select linear 0.1s";
+				}
+				else
+					enemySelectedImage.src="images/"+selected+"-hover.png";
+		},i*100,i==9);
 	}
 }
